ef83bdedaf927873c5f594e498d2408b2428a1d2
kual Louis <mubaraklouis@gmail.com>
2023-05-07 12:37:14 +0200

    done with sounds and leveling up

--- 2d-break-game/game.js
+++ 2d-break-game/game.js
@@ -14,6 +14,11 @@ const PADDLE_MARGIN_BOTTOM = 50;
 const PADDLE_HEIGHT = 20;
 const BALL_RADIUS = 8;
 let LIFE = 3; // PLAYER HAS 3 LIVES
+let SCORE = 0;
+const SCORE_UNIT = 10;
+let LEVEL = 1;
+const MAX_LEVEL = 3;
+let GAME_OVER = false;
 let leftArrow = false;
 let rightArrow = false;
 
@@ -94,14 +99,17 @@ function moveBall(){
 function ballWallCollision(){
     if(ball.x + ball.radius > cvs.width || ball.x - ball.radius < 0){
         ball.dx = - ball.dx;
+        WALL_HIT.play();
     }
     
     if(ball.y - ball.radius < 0){
         ball.dy = -ball.dy;
+        WALL_HIT.play();
     }
     
     if(ball.y + ball.radius > cvs.height){
         LIFE--; // LOSE LIFE
+        LIFE_LOST.play();
         resetBall();
     }
 }
@@ -118,6 +126,9 @@ function resetBall(){
 function ballPaddleCollision(){
     if(ball.x < paddle.x + paddle.width && ball.x > paddle.x && paddle.y < paddle.y + paddle.height && ball.y > paddle.y){
         
+        // PLAY SOUND
+        PADDLE_HIT.play();
+        
         // CHECK WHERE THE BALL HIT THE PADDLE
         let collidePoint = ball.x - (paddle.x + paddle.width/2);
         
@@ -133,8 +144,7 @@ function ballPaddleCollision(){
     }
 }
 
-//CREATE THE BRICK OBJECT
-
+// CREATE THE BRICKS
 const brick = {
     row : 1,
     column : 5,
@@ -145,34 +155,14 @@ const brick = {
     marginTop : 40,
     fillColor : "#2e3548",
     strokeColor : "#FFF"
-} 
- //CREATE THE BRICK ARRAYS THAT STORE THE BRICKS POSITIONS
-    
-    let bricks =[];
-    
-    //CREATE THE POSITION OF THE BRICKS
-    for(let r =0; r < brick.row; r++){
-    
-    //FILL THE BRICKS  POSITIONS
-    bricks[r]=[]
-    for(let c = 0; c< brick.column; c++){
-    bricks[r][c]={
-    x:c*(brick.offSetLeft + brick.width)+brick.offSetLeft,
-    y:45,
-    status:true
-    }
-    }
-    
-    }
+}
 
+let bricks = [];
 
-    createBricks();
-
- //DRAW BRICKS FUNCTION
- function createBricks(){
-    for(let r = 0; r < brick.rows; r++){
+function createBricks(){
+    for(let r = 0; r < brick.row; r++){
         bricks[r] = [];
-        for(let c = 0; c < brick.coloumns; c++){
+        for(let c = 0; c < brick.column; c++){
             bricks[r][c] = {
                 x : c * ( brick.offSetLeft + brick.width ) + brick.offSetLeft,
                 y : r * ( brick.offSetTop + brick.height ) + brick.offSetTop + brick.marginTop,
@@ -182,8 +172,10 @@ const brick = {
     }
 }
 
+createBricks();
 
- function drawBricks(){
+// draw the bricks
+function drawBricks(){
     for(let r = 0; r < brick.row; r++){
         for(let c = 0; c < brick.column; c++){
             let b = bricks[r][c];
@@ -199,12 +191,84 @@ const brick = {
     }
 }
 
+// ball brick collision
+function ballBrickCollision(){
+    for(let r = 0; r < brick.row; r++){
+        for(let c = 0; c < brick.column; c++){
+            let b = bricks[r][c];
+            // if the brick isn't broken
+            if(b.status){
+                if(ball.x + ball.radius > b.x && ball.x - ball.radius < b.x + brick.width && ball.y + ball.radius > b.y && ball.y - ball.radius < b.y + brick.height){
+                    BRICK_HIT.play();
+                    ball.dy = - ball.dy;
+                    b.status = false; // the brick is broken
+                    SCORE += SCORE_UNIT;
+                }
+            }
+        }
+    }
+}
+
+// show game stats
+function showGameStats(text, textX, textY, img, imgX, imgY){
+    // draw text
+    ctx.fillStyle = "#FFF";
+    ctx.font = "25px Germania One";
+    ctx.fillText(text, textX, textY);
+    
+    // draw image
+    ctx.drawImage(img, imgX, imgY, width = 25, height = 25);
+}
+
 // DRAW FUNCTION
 function draw(){
     drawPaddle();
     
     drawBall();
+    
     drawBricks();
+    
+    // SHOW SCORE
+    showGameStats(SCORE, 35, 25, SCORE_IMG, 5, 5);
+    // SHOW LIVES
+    showGameStats(LIFE, cvs.width - 25, 25, LIFE_IMG, cvs.width-55, 5); 
+    // SHOW LEVEL
+    showGameStats(LEVEL, cvs.width/2, 25, LEVEL_IMG, cvs.width/2 - 30, 5);
+}
+
+// game over
+function gameOver(){
+    if(LIFE <= 0){
+        showYouLose();
+        GAME_OVER = true;
+    }
+}
+
+// level up
+function levelUp(){
+    let isLevelDone = true;
+    
+    // check if all the bricks are broken
+    for(let r = 0; r < brick.row; r++){
+        for(let c = 0; c < brick.column; c++){
+            isLevelDone = isLevelDone && ! bricks[r][c].status;
+        }
+    }
+    
+    if(isLevelDone){
+        WIN.play();
+        
+        if(LEVEL >= MAX_LEVEL){
+            showYouWin();
+            GAME_OVER = true;
+            return;
+        }
+        brick.row++;
+        createBricks();
+        ball.speed += 0.5;
+        resetBall();
+        LEVEL++;
+    }
 }
 
 // UPDATE GAME FUNCTION
@@ -216,33 +280,73 @@ function update(){
     ballWallCollision();
     
     ballPaddleCollision();
+    
+    ballBrickCollision();
+    
+    gameOver();
+    
+    levelUp();
 }
-
-
-
 
 // GAME LOOP
 function loop(){
     // CLEAR THE CANVAS
     ctx.drawImage(BG_IMG, 0, 0);
-    //the draw function is responsible for drawing objects on the canvas
+    
     draw();
-    //the update function  dills with updating the moving paths of the game
+    
     update();
-    //this is the main game loop controls frame rate of the game
-    requestAnimationFrame(loop);
-
-
+    
+    if(! GAME_OVER){
+        requestAnimationFrame(loop);
+    }
 }
 loop();
 
 
-// things to figure out 
-//debug the bricks viewing on the screen
+// SELECT SOUND ELEMENT
+const soundElement  = document.getElementById("sound");
 
+soundElement.addEventListener("click", audioManager);
 
+function audioManager(){
+    // CHANGE IMAGE SOUND_ON/OFF
+    let imgSrc = soundElement.getAttribute("src");
+    let SOUND_IMG = imgSrc == "img/SOUND_ON.png" ? "img/SOUND_OFF.png" : "img/SOUND_ON.png";
+    
+    soundElement.setAttribute("src", SOUND_IMG);
+    
+    // MUTE AND UNMUTE SOUNDS
+    WALL_HIT.muted = WALL_HIT.muted ? false : true;
+    PADDLE_HIT.muted = PADDLE_HIT.muted ? false : true;
+    BRICK_HIT.muted = BRICK_HIT.muted ? false : true;
+    WIN.muted = WIN.muted ? false : true;
+    LIFE_LOST.muted = LIFE_LOST.muted ? false : true;
+}
 
+// SHOW GAME OVER MESSAGE
+/* SELECT ELEMENTS */
+const gameover = document.getElementById("gameover");
+const youwin = document.getElementById("youwin");
+const youlose = document.getElementById("youlose");
+const restart = document.getElementById("restart");
 
+// CLICK ON PLAY AGAIN BUTTON
+restart.addEventListener("click", function(){
+    location.reload(); // reload the page
+})
+
+// SHOW YOU WIN
+function showYouWin(){
+    gameover.style.display = "block";
+    youwon.style.display = "block";
+}
+
+// SHOW YOU LOSE
+function showYouLose(){
+    gameover.style.display = "block";
+    youlose.style.display = "block";
+}
 
 
 
